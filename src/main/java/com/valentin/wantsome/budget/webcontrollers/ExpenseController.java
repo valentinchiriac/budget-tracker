fdcc33bb4c78d1f67aa786ada7e085b71e73b9de
valentinchiriac <toader.chiriac@gmail.com>
2025-05-09 18:36:39 +0300

    income and expenses html forms

--- src/main/java/com/valentin/wantsome/budget/webcontrollers/ExpenseController.java
+++ src/main/java/com/valentin/wantsome/budget/webcontrollers/ExpenseController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
-
 @Controller
 public class ExpenseController {
     private final ExpenseService expenseService;
